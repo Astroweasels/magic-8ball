db0f8f454a4e04369cbe2793cd0b7f81cf9f1b58
Kole Petersen <kole_duran@hotmail.com>
2023-03-16 16:22:57 -0500

    yay

--- src/script.js
+++ src/script.js
@@ -4,6 +4,6 @@ function generateRandomNumberBetween(number) {
 }
 
 function generate8BallMessage() {
-    let messages = ["Yes", "No", "Maybe", "Shake Again", "Prepare to Die", "Omae Wa Mou Shin De Ru"];
+    let messages = ["Yes", "No", "Maybe", "Shake Again","Kyle is a bitch" , "Prepare to Die", "Omae Wa Mou Shin De Ru"];
     document.getElementById("8-ball-message").innerHTML = messages[generateRandomNumberBetween(messages.length)];
 }
